44b1d7fbf74d18c9b9164111f73c190e426ada70
fis <jm.yuan@outlook.com>
2020-10-31 13:48:13 +0800

    Basic CPU test.

--- tests/cpp/common/test_common.cc
+++ tests/cpp/common/test_common.cc
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
+#include <xgboost/host_device_vector.h>
+#include <xgboost/generic_parameters.h>
 #include "../../../src/common/common.h"
+#include "../helpers.h"
 
 namespace xgboost {
 namespace common {
@@ -8,6 +11,19 @@ TEST(ArgSort, Basic) {
   auto ret = ArgSort<bst_feature_t>(inputs);
   std::vector<bst_feature_t> sol{2, 1, 0};
   ASSERT_EQ(ret, sol);
+}
+
+TEST(StridedCopy, Basic) {
+  auto g = GenerateRandomGradients(100);
+  ASSERT_EQ(g.DeviceIdx(), GenericParameter::kCpuId);
+  HostDeviceVector<GradientPair> out(100);
+
+  StridedCopy(&out, g, 1, 1);
+
+  HostDeviceVector<GradientPair> solution(100);
+  solution.Copy(g);
+
+  ASSERT_EQ(solution.HostVector(), g.HostVector());
 }
 }  // namespace common
 }  // namespace xgboost

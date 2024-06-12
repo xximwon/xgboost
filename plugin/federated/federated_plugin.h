4a4d04a065281f498fefa730d1b53ed68a0eb137
Jiaming Yuan <jm.yuan@outlook.com>
2024-06-13 02:33:56 +0800

    pull in the mock.

--- plugin/federated/federated_plugin.h
+++ plugin/federated/federated_plugin.h
@@ -39,10 +39,105 @@ using SyncHistHoriFn = int(FederatedPluginHandle handle, std::uint8_t const *buf
                            std::size_t len, double **out_hist, std::size_t *out_len);
 }  // namespace federated
 
+// Base class, only used for testing.
+class FederatedPluginBase {
+  std::vector<std::uint8_t> grad_;
+  std::vector<std::uint8_t> hist_enc_;
+  std::vector<double> hist_plain_;
+
+ public:
+  [[nodiscard]] virtual common::Span<std::uint8_t> EncryptGradient(common::Span<float const> data) {
+    grad_.resize(data.size_bytes());
+    auto casted =
+        common::Span{reinterpret_cast<std::uint8_t const *>(data.data()), data.size_bytes()};
+    std::copy_n(casted.data(), casted.size(), grad_.data());
+    return grad_;
+  }
+  virtual void SyncEncryptedGradient(common::Span<std::uint8_t const>) {
+    // nothing
+  }
+
+  // Vertical histogram
+  virtual void Reset(common::Span<std::uint32_t const>, common::Span<std::int32_t const>) {}
+
+  [[nodiscard]] virtual common::Span<std::uint8_t> BuildEncryptedHistVert(
+      common::Span<std::uint64_t const *> rowptrs, common::Span<std::size_t const> sizes,
+      common::Span<bst_node_t const> nids) {
+    int total_bin_size = cuts_.back();
+    int histo_size = total_bin_size * 2;
+    *size = kPrefixLen + 8 * histo_size * nodes.size();
+    int64_t buf_size = *size;
+    int8_t *buf = static_cast<int8_t *>(calloc(buf_size, 1));
+    memcpy(buf, kSignature, strlen(kSignature));
+    memcpy(buf + 8, &buf_size, 8);
+    memcpy(buf + 16, &kDataTypeHisto, 8);
+
+    double *histo = reinterpret_cast<double *>(buf + kPrefixLen);
+    for (const auto &node : nodes) {
+      auto rows = node.second;
+      for (const auto &row_id : rows) {
+        auto num = cuts_.size() - 1;
+        for (std::size_t f = 0; f < num; f++) {
+          int slot = slots_[f + num * row_id];
+          if ((slot < 0) || (slot >= total_bin_size)) {
+            continue;
+          }
+
+          auto g = (*gh_pairs_)[row_id * 2];
+          auto h = (*gh_pairs_)[row_id * 2 + 1];
+          histo[slot * 2] += g;
+          histo[slot * 2 + 1] += h;
+        }
+      }
+      histo += histo_size;
+    }
+
+    return buf;
+  }
+
+  [[nodiscard]] virtual common::Span<double> SyncEncryptedHistVert(
+      common::Span<std::uint8_t> hist) {
+    std::vector<double> result = std::vector<double>();
+
+    int8_t *ptr = static_cast<int8_t *>(buffer);
+    auto rest_size = buf_size;
+
+    while (rest_size > kPrefixLen) {
+      if (!ValidDam(ptr, rest_size)) {
+        break;
+      }
+      int64_t *size_ptr = reinterpret_cast<int64_t *>(ptr + 8);
+      double *array_start = reinterpret_cast<double *>(ptr + kPrefixLen);
+      auto array_size = (*size_ptr - kPrefixLen) / 8;
+      result.insert(result.end(), array_start, array_start + array_size);
+      rest_size -= *size_ptr;
+      ptr = ptr + *size_ptr;
+    }
+
+    return result;
+  }
+
+  // Horizontal histogram
+  [[nodiscard]] virtual common::Span<std::uint8_t> BuildEncryptedHistHori(
+      common::Span<double const> hist) {
+    hist_enc_.resize(hist.size_bytes());
+    std::copy_n(reinterpret_cast<std::uint8_t const *>(hist.data()), hist.size_bytes(),
+                hist_enc_.data());
+    return hist_enc_;
+  }
+  [[nodiscard]] virtual common::Span<double> SyncEncryptedHistHori(
+      common::Span<std::uint8_t const> hist) {
+    hist_plain_.resize(hist.size_bytes());
+    std::copy_n(hist.data(), hist.size_bytes(),
+                reinterpret_cast<std::uint8_t const *>(hist_plain_.data()));
+    return hist_plain_;
+  }
+};
+
 /**
  * @brief Bridge for plugins that handle encryption.
  */
-class FederatedPlugin {
+class FederatedPlugin : public FederatedPluginBase {
   // Federated plugin shared object, for dlopen.
   std::unique_ptr<void, std::function<void(void *)>> plugin_;
 
@@ -74,14 +169,15 @@ class FederatedPlugin {
   explicit FederatedPlugin(std::string_view path, Json config);
   ~FederatedPlugin();
   // Gradient
-  [[nodiscard]] virtual common::Span<std::uint8_t> EncryptGradient(common::Span<float const> data) {
+  [[nodiscard]] common::Span<std::uint8_t> EncryptGradient(
+      common::Span<float const> data) override {
     std::uint8_t *ptr{nullptr};
     std::size_t n{0};
     auto rc = Encrypt_(this->plugin_handle_.get(), data.data(), data.size(), &ptr, &n);
     CheckRC(rc, "Failed to encrypt gradient");
     return {ptr, n};
   }
-  virtual void SyncEncryptedGradient(common::Span<std::uint8_t const> data) {
+  void SyncEncryptedGradient(common::Span<std::uint8_t const> data) override {
     uint8_t const *out;
     std::size_t n{0};
     auto rc = SyncEncrypt_(this->plugin_handle_.get(), data.data(), data.size(), &out, &n);
@@ -89,15 +185,15 @@ class FederatedPlugin {
   }
 
   // Vertical histogram
-  virtual void Reset(common::Span<std::uint32_t const> cutptrs,
-                     common::Span<std::int32_t const> bin_idx) {
+  void Reset(common::Span<std::uint32_t const> cutptrs,
+             common::Span<std::int32_t const> bin_idx) override {
     auto rc = ResetHistCtxVert_(this->plugin_handle_.get(), cutptrs.data(), cutptrs.size(),
                                 bin_idx.data(), bin_idx.size());
     CheckRC(rc, "Failed to set the data context for federated learning");
   }
-  [[nodiscard]] virtual common::Span<std::uint8_t> BuildEncryptedHistVert(
+  [[nodiscard]] common::Span<std::uint8_t> BuildEncryptedHistVert(
       common::Span<std::uint64_t const *> rowptrs, common::Span<std::size_t const> sizes,
-      common::Span<bst_node_t const> nids) {
+      common::Span<bst_node_t const> nids) override {
     std::uint8_t *ptr{nullptr};
     std::size_t n{0};
     auto rc = BuildEncryptedHistVert_(this->plugin_handle_.get(), rowptrs.data(), sizes.data(),
@@ -105,8 +201,8 @@ class FederatedPlugin {
     CheckRC(rc, "Failed to build the encrypted hist");
     return {ptr, n};
   }
-  [[nodiscard]] virtual common::Span<double> SyncEncryptedHistVert(
-      common::Span<std::uint8_t> hist) {
+  [[nodiscard]] common::Span<double> SyncEncryptedHistVert(
+      common::Span<std::uint8_t> hist) override {
     double *ptr{nullptr};
     std::size_t n{0};
     auto rc =
@@ -116,8 +212,8 @@ class FederatedPlugin {
   }
 
   // Horizontal histogram
-  [[nodiscard]] virtual common::Span<std::uint8_t> BuildEncryptedHistHori(
-      common::Span<double const> hist) {
+  [[nodiscard]] common::Span<std::uint8_t> BuildEncryptedHistHori(
+      common::Span<double const> hist) override {
     std::uint8_t *ptr{nullptr};
     std::size_t n{0};
     auto rc =
@@ -125,8 +221,8 @@ class FederatedPlugin {
     CheckRC(rc, "Failed to build the encrypted hist");
     return {ptr, n};
   }
-  [[nodiscard]] virtual common::Span<double> SyncEncryptedHistHori(
-      common::Span<std::uint8_t const> hist) {
+  [[nodiscard]] common::Span<double> SyncEncryptedHistHori(
+      common::Span<std::uint8_t const> hist) override {
     double *ptr{nullptr};
     std::size_t n{0};
     auto rc =
@@ -134,9 +230,5 @@ class FederatedPlugin {
     CheckRC(rc, "Failed to sync the encrypted hist");
     return {ptr, n};
   }
-};
-
-class FederatedPluginMock : public FederatedPlugin {
- public:
 };
 }  // namespace xgboost::collective

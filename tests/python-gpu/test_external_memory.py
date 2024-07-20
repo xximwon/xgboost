64542b208158ab2768bd1c0535bd1c00abc4f6bd
Jiaming Yuan <jm.yuan@outlook.com>
2024-07-21 04:40:04 +0800

    27

--- tests/python-gpu/test_external_memory.py
+++ tests/python-gpu/test_external_memory.py
@@ -10,7 +10,7 @@ def main() -> None:
         os.mkdir(data_dir)
     # run_external_memory(data_dir, reuse=False, on_host=True, n_samples_per_batch=2**22)
     # run_over_subscription(data_dir, True, n_bins=256, n_samples=2**26 + 2**24)
-    run_over_subscription(data_dir, True, n_bins=256, n_samples=2**24, is_sam=True)
+    run_over_subscription(data_dir, True, n_bins=256, n_samples=2**27, is_sam=True)
     # run_over_subscription(data_dir, True, n_bins=26, n_samples=2**23, is_sam=False)
 
 
